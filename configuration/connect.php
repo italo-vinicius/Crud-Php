68a928431b8d80d54500e80f857114a0e7849954
Italo Vinicius <italoviniciusrope@gmail.com>
2023-04-21 23:34:14 -0300

    add connect pdo & add .env

--- configuration/connect.php
+++ configuration/connect.php
@@ -1,1 +1,34 @@
 <?php
+
+namespace Connect;
+require_once __DIR__ . '/../vendor/autoload.php';
+
+
+use PDO;
+use PDOException;
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__  . '/..');
+$dotenv->load();
+
+
+
+class Connect
+{
+    protected $connection;
+
+    public function __construct()
+    {
+        $this->connectDatabase();
+    }
+
+    private function connectDatabase(): void
+    {
+        try {
+            $this->connection = new PDO('mysql:host=' . getenv('HOST') . ';dbname=' . getenv('DATABASE'), getenv('USER'), getenv('PASSWORD'));
+        } catch (PDOException $e) {
+            echo 'ERROR! ' . $e->getMessage();
+            die();
+        }
+    }
+}
